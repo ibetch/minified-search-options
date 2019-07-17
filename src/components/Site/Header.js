5452f53cd9af59f932e0dbbb508e6489991ffdcc
Khristine Tugangui <ibetch@gmail.com>
2019-07-17 13:57:51 -0700

    refactor(sitesearch): reorg dir structure

--- src/components/Site/Header.js
+++ src/components/Site/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
-import SearchForm from '../SearchForm';
+import SiteSearch from '../SiteSearch';
 import styles from './header.module';
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={`dn dib-m ${styles.nav} `}>
           <Nav/>
         </div>
-        <div className={styles.search}><SearchForm /></div>
+        <div className={styles.search}><SiteSearch /></div>
         <div className={`${styles.mobileNav}`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="3 3 18 18" className="svgIcon dib vam"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
         </div>
